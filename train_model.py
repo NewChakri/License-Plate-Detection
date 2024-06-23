315ad8f14bd84c2dc491e3450e140c76bd8d459e
NewChakri <99199609+NewChakri@users.noreply.github.com>
2024-06-23 09:15:26 +0700

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -3,5 +3,8 @@ from ultralytics import YOLO
 # Load a pre-trained YOLOv9 model
 model = YOLO('yolov9c.pt')
 
-# Train the model on your dataset
+# Train the model on custom dataset
 results = model.train(data='data.yaml', epochs=50, imgsz=640)
+
+# Evaluate the model
+metrics = model.val()
